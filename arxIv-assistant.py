455520cb4915a2d5a320ef50fa8b37cbaf866165
rixmape <rixdonninorecario.mape@bicol-u.edu.ph>
2023-09-13 17:07:24 +0800

    Update for basic streamlit app with memory

--- arxIv-assistant.py
+++ arxIv-assistant.py
@@ -13,3 +13,63 @@ Psuedocode:
 8. Display the response.
 9. Repeat steps 1-8 until the user quits.
 """
+
+import streamlit as st
+from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+from langchain.prompts import PromptTemplate
+
+st.set_page_config(page_title="StreamlitChatMessageHistory", page_icon="📖")
+st.title("📖 StreamlitChatMessageHistory")
+
+# Set up memory
+msgs = StreamlitChatMessageHistory(key="langchain_messages")
+memory = ConversationBufferMemory(chat_memory=msgs)
+
+# Set up expander to view the messages stored in memory
+view_messages = st.expander("View the message contents in session state")
+
+# Add a default message if there are no messages in memory
+if len(msgs.messages) == 0:
+    msgs.add_ai_message("How can I help you?")
+
+# Render current messages from StreamlitChatMessageHistory
+for msg in msgs.messages:
+    st.chat_message(msg.type).write(msg.content)
+
+# Set up the LLMChain, passing in memory
+template = """You are an AI chatbot having a conversation with a human.
+
+{history}
+Human: {input}
+AI: """
+
+prompt = PromptTemplate(
+    input_variables=["history", "input"],
+    template=template,
+)
+
+llm = ChatOpenAI(temperature=0)
+conversation = ConversationChain(llm=llm, memory=memory, prompt=prompt)
+
+# If user inputs a new prompt, generate and draw a new response
+# New messages are added to memory automatically
+if query := st.chat_input():
+    st.chat_message("human").write(query)
+    response = conversation.predict(input=query)
+    st.chat_message("ai").write(response)
+
+# Draw the messages at the end, so newly generated ones show up immediately
+with view_messages:
+    """
+    Memory initialized with:
+    ```python
+    msgs = StreamlitChatMessageHistory(key="langchain_messages")
+    memory = ConversationBufferMemory(chat_memory=msgs)
+    ```
+
+    Contents of `st.session_state.langchain_messages`:
+    """
+    view_messages.json(st.session_state.langchain_messages)
