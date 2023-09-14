fe61bff3338a2949f90bea61e81a6aa59b2aac74
rixmape <rixdonninorecario.mape@bicol-u.edu.ph>
2023-09-14 11:37:38 +0800

    Refactor arXiv assistant code and remove redundant files
    - Refactored the `arxiv-assistant/main.py` file to integrate LangChain, a Python framework for large language models (LLMs), and arXiv, a repository for e-prints of scientific papers, to create a question answering program.
    - Removed the `arxiv-assistant/main.py`, `arxiv-assistant/templates.py` files as they are no longer needed.

--- arxiv-agent-with-memory.py
+++ arxiv-agent-with-memory.py
@@ -1,3 +1,19 @@
+"""
+This module integrates LangChain, a Python framework for large language models (LLMs), and arXiv, a repository for e-prints of scientific papers, to create a question answering program. The goal is to demonstrate how to use LangChain and related libraries in building LLM-powered applications.
+
+Here's the plan:
+
+1. Ask the user for a query.
+2. Expand the query by adding related words.
+3. Use the expanded query to search arXiv for papers.
+5. Create embeddings from the papers' abstracts.
+6. Create embeddings from the orginal query.
+6. Find the most relevant papers to the orginal query.
+7. Feed the retrieved abstracts and the query to GPT-3.
+8. Display the response.
+9. Repeat steps 1-8 until the user quits.
+"""
+
 import streamlit as st
 
 from langchain.agents import AgentType, initialize_agent, load_tools
@@ -24,7 +40,12 @@ memory = ConversationBufferMemory(
 
 # Add a default message if there are no messages in memory
 if len(msgs.messages) == 0:
-    msgs.add_ai_message("How can I help you?")
+    msgs.add_ai_message(
+        "As a helpful research assistant, I can answer questions using "
+        "research papers available on arXiv. I also have access to a few "
+        "other tools that can help me answer your questions. **Try asking me "
+        "something!**"
+    )
 
 # Render current messages from StreamlitChatMessageHistory
 view_messages = st.expander("View the message contents in session state")
